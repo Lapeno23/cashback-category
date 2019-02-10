31ac8d3af1cfcdc2cc9bdb44201af2681a02ee12
Lapeno23 <RVYakovenko@stud.kai.ru>
2019-02-10 10:28:17 +0300

    Create sql scripts

---
+++ src/main/java/ru/itpark/CashbackCategoryApplication.java
@@ -0,0 +1,14 @@
+package ru.itpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashbackCategoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashbackCategoryApplication.class, args);
+    }
+
+}
+
---
+++ src/main/resources/sql/banksdata-h2.sql
@@ -0,0 +1,4 @@
+INSERT INTO banks (id, name)
+                      VALUES (1, 'Tinkoff'),
+                             (2, 'AkBars'),
+                             (3, 'Megafon');
